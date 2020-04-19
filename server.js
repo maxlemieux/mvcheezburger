f645bad3abd915931782365c54a38df3d7283786
Max Lemieux <max.lemieux@gmail.com>
2020-04-18 19:11:18 -0700

    added comments

--- server.js
+++ server.js
@@ -1,22 +1,26 @@
 require('dotenv').config()
-
 const PORT = process.env.PORT || 3000;
 
+/* Express */
 const express = require("express");
 const app = express();
+
+/* Middleware */
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+/* Handlebars */
 const exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
+/* Routes */
 const routes = require("./controllers/burgers_controller.js");
 app.use(routes);
 
+/* Start the server */
 app.listen(PORT, function() {
-  console.log("App now listening at localhost:" + PORT);
+  console.log(`App now listening at localhost: ${PORT}`);
 });
